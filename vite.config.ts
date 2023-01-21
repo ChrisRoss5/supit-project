a07ed19a83c78495c040d43fd65f5f7a9bf654c3
Chris <kristijan.ros@gmail.com>
2023-01-21 03:53:15 +0100

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    commonjsOptions: {
-      include: ["tailwind-config.cjs", "node_modules/**"],
-    },
-  },
-  optimizeDeps: {
-    include: ["tailwind-config"],
-  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
-      "tailwind-config": resolve(__dirname, "./tailwind.config.cjs"),
     },
   },
 });

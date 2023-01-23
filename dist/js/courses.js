16e91b67ebd5039670faa138a80915c19f8f19d6
Chris <kristijan.ros@gmail.com>
2023-01-23 13:49:17 +0100

    init

--- dist/js/courses.js
+++ dist/js/courses.js
@@ -1,6 +1,7 @@
 /* https://api.jqueryui.com/autocomplete/ */
 
 import { signOut } from "./auth.js";
+import { loadScript, loadStylesheet } from "./utils.js";
 
 const coursesURL = "https://www.fulek.com/data/api/supit/curriculum-list/hr";
 const courseURL = "https://www.fulek.com/data/api/supit/get-curriculum";
@@ -40,6 +41,15 @@ const init = async () => {
       $("#" + val).text(selectedCourses.reduce((a, b) => a + b[val], 0));
     $("#nastavni-plan table")[selectedCourses.length ? "show" : "hide"]();
   };
+
+  loadStylesheet("https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css");
+  // no SRI & crossorigin attributes, but also works instead of loadScript():
+  // await import("https://code.jquery.com/ui/1.13.2/jquery-ui.min.js");
+  await loadScript({
+    src: "https://code.jquery.com/ui/1.13.2/jquery-ui.min.js",
+    integrity: "sha256-lSjKY0/srUM9BE3dPm+c4fBo1dky2v27Gdjm2uoZaL0=",
+    crossorigin: "anonymous",
+  });
 
   $("#nastavni-plan input").autocomplete({
     source: courses.map((i) => i.kolegij),
--- dist/js/utils.js
+++ dist/js/utils.js
@@ -11,3 +11,22 @@ export const disableButton = (button) => {
     .removeClass("btn-dark")
     .addClass("btn-outline-dark");
 };
+
+export const loadStylesheet = (href) => {
+  if (document.querySelector(`link[href="${href}"]`)) return;
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = href;
+  document.head.appendChild(link);
+};
+
+export const loadScript = async (attrs) => {
+  return new Promise((resolve) => {
+    if (document.querySelector(`script[src="${attrs.src}"]`))
+      return resolve(true);
+    const script = document.createElement("script");
+    for (const key in attrs) script.setAttribute(key, attrs[key]);
+    script.onload = resolve;
+    document.head.appendChild(script);
+  });
+};

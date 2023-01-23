a8a1e88b8868a32fe25badd4e9effc4b97bdf30c
Chris <kristijan.ros@gmail.com>
2023-01-23 23:47:59 +0100

    init

--- README.md
+++ README.md
@@ -1,40 +1,118 @@
-# Visit [supit.k1k1.dev](https://supit.k1k1.dev/) to explore all versions!
+## Visit [supit.k1k1.dev](https://supit.k1k1.dev/) to explore all versions!
+
 Hosted on Cloudflare with automatic `build` per branch on `push` or `merge`.
 
 This branch (supit.k1k1.dev) is built with vanilla JS & custom CSS.
 
+It's using [PageSpeed Insights API](https://developers.google.com/speed/docs/insights/v5/about) to provide the most consistent scores from [Lighthouse](https://developer.chrome.com/docs/lighthouse/) audits.
+
 ## Installation
+
 Don't forget to `npm i` before you `npm run dev` after switching to a branch with `package.json`.
 
-## Comparisons (ordered by completion).
+All versions are configured to `npm run build` in `/dist` folder.
+
+## Comparisons (ordered by completion)
 
 ### jQuery
+
 Has more functionality at `/o-nama` and `/novost/1`
+
 - because it utilizes `bootstrap.ScrollSpy`, which highlights navbar links based on the most visible section.
 - because it utilizes `bootstrap.Carousel`, which supports swiping on touch devices.
 
 Note that there's no `build` process.
 
-DX: Will not use **jQuery** NOR **Bootstrap** ever again.
+DX: Will not use **jQuery** nor **Bootstrap** again.
 
 ### Vue
+
 DX: Simply _the best_.
 
 ### React
+
 DX: Overcomplicated with too much boilerplate.
 
 ### Svelte
-DX: Almost as good as **Vue**, but the lack of build configs & lighthouse performance is disappointing.
-
-There's a reason why the performance is not as ideal, mentioned in [SvelteKit Docs](https://kit.svelte.dev/docs/adapter-static#spa-mode).
 
 Unlike **Vue** and **React**, there's no support for `HeadlessUI`
 so I had to bring functionality to `modal` and `autocomplete` components myself.
 Apart from that, all **Vue**, **React** & **Svelte** functionality and design should look 100% identical from a user's perspective.
 
+DX: Almost as good as **Vue**.
+
+## Comparisons (performance)
+
+These scores turn out to be the most common for **landing pages**:
+
+<table>
+  <tr>
+    <th rowspan=2>Version</th>
+    <th colspan=3>Mobile</th>
+    <th colspan=3>Desktop</th>
+    <th rowspan=2>Resource Bytes</th>
+    <th rowspan=2>Files/Chunks</th>
+  </tr>
+  <tr>
+    <th>Performance</th>
+    <th>Accessibility</th>
+    <th>Best Practices</th>
+    <th>Performance</th>
+    <th>Accessibility</th>
+    <th>Best Practices</th>
+  </tr>
+  <tr>
+    <td>jQuery</td>
+    <td style="text-align: center">95</td>
+    <td style="text-align: center">100</td>
+    <td style="text-align: center">100</td>
+    <td style="text-align: center">99</td>
+    <td style="text-align: center">100</td>
+    <td style="text-align: center">100</td>
+    <td style="text-align: center">169 KiB</td>
+    <td style="text-align: center">5 files</td>
+  </tr>
+  <tr>
+    <td>Vue</td>
+    <td style="text-align: center">87</td>
+    <td style="text-align: center">100</td>
+    <td style="text-align: center">100</td>
+    <td style="text-align: center">99</td>
+    <td style="text-align: center">100</td>
+    <td style="text-align: center">100</td>
+    <td style="text-align: center">243 KiB</td>
+    <td style="text-align: center">1 chunk</td>
+  </tr>
+  <tr>
+    <td>React</td>
+    <td style="text-align: center">87</td>
+    <td style="text-align: center">100</td>
+    <td style="text-align: center">100</td>
+    <td style="text-align: center">99</td>
+    <td style="text-align: center">100</td>
+    <td style="text-align: center">100</td>
+    <td style="text-align: center">310 KiB</td>
+    <td style="text-align: center">1 chunk</td>
+  </tr>
+  <tr>
+    <td>Svelte</td>
+    <td style="text-align: center">89</td>
+    <td style="text-align: center">100</td>
+    <td style="text-align: center">100</td>
+    <td style="text-align: center">99</td>
+    <td style="text-align: center">100</td>
+    <td style="text-align: center">100</td>
+    <td style="text-align: center">72 KiB</td>
+    <td style="text-align: center">15 chunks</td>
+  </tr>
+</table>
+
+You can find file sizes & count by pressing the `View Treemap` button below the metrics of Lighthouse audits.
+
 ## Todo
 
-[Make SvelteKit bundle all chunks into a single bundle](https://www.reddit.com/r/sveltejs/comments/rqo5o2/make_sveltekit_bundle_all_ts_files_into_a_single/) (no fix yet)
+- Angular version with Webpack
+- [Make SvelteKit bundle all chunks into a single bundle](https://www.reddit.com/r/sveltejs/comments/rqo5o2/make_sveltekit_bundle_all_ts_files_into_a_single/) (no solution yet)
 
 ## Good to know
 
---
+++ dist/main.js
@@ -0,0 +1,132 @@
+/* https://developers.google.com/speed/docs/insights/v5/get-started#javascript */
+
+const useAPI = true;
+const APIkey = "AIzaSyCuC1VTP2JRefN98l8e1jA6Ga8OIPXq2LE";
+const devices = ["mobile", "desktop"];
+const categories = ["Performance", "Accessibility", "Best Practices"];
+const projects = [
+  {
+    name: "jquery",
+    type: "Lazy Loaded · Resource Bytes: 169 KiB (418 KiB with jQueryUI), 5 files",
+    tech: "JavaScript modules, jQuery + jQuery UI, Bootstrap + Bootstrap Icons",
+  },
+  {
+    name: "vue",
+    type: "Fully Client Side Rendered · Resource Bytes: 243 KiB, 1 chunk",
+    tech:
+      "Vite, TypeScript, PostCSS, Tailwind + HeadlessUI, Google Material Icons,<br>" +
+      "Vue + Vue Router + Pinia",
+  },
+  {
+    name: "react",
+    type: "Fully Client Side Rendered · Resource Bytes: 310 KiB, 1 chunk",
+    tech:
+      "Vite, TypeScript, PostCSS, Tailwind + HeadlessUI, Google Material Icons,<br>" +
+      "React + React Redux + React Router + React Transition Group",
+  },
+  {
+    name: "svelte",
+    type: "Lazy Loaded · Resource Bytes: 72 KiB, 15 chunks",
+    tech: "Vite, TypeScript, PostCSS, Tailwind, Svelte + SvelteKit, Google Material Icons",
+  },
+];
+
+const content = projects.map(getProject).join("");
+document.body.insertAdjacentHTML("beforeend", content);
+
+function getProject({ name, type, tech }, i) {
+  if (useAPI) {
+    /* const audits = [[50, 100, 100], [100, 100, 100]]; */
+    const audits = devices.map((device) => runPagespeedAPI(name, device));
+    Promise.all(audits).then((results) => {
+      results.forEach((percentages, j) => {
+        const scoresEl = document.querySelector(`[data-${i}] [data-${j}]`);
+        const loaderEl = scoresEl.firstElementChild;
+        loaderEl.style.opacity = 0;
+        setTimeout(() => {
+          percentages.forEach((p, k) => {
+            setTimeout(() => {
+              updateGauge(scoresEl.children[k + 1], p);
+            }, (j * percentages.length + k) * 150);
+          });
+        }, 300);
+      });
+    });
+  }
+
+  return /* html */ `
+    <div class="project" style="animation: reset 1s ${i * 150}ms forwards">
+      <a
+        class="main-link"
+        href="https://${name}.supit.k1k1.dev/"
+        target="_blank"
+      >
+        <h2><img src="/icons/${name}.svg" alt="icon" />${name}.supit.k1k1.dev</h2>
+        <p>${type}</p>
+        ${tech}
+      </a>
+      <div class="reports" data-${i}>
+        ${devices.map(getReport.bind({ name })).join("")}
+      </div>
+    </div>`;
+}
+
+function getReport(device, j) {
+  const href =
+    "https://pagespeed.web.dev/report?url=https%3A%2F%2F" +
+    `${this.name}.supit.k1k1.dev%2F&form_factor=${device}`;
+
+  return /* html */ `
+    <a href="${href}" target="_blank">
+      <div class="scores" data-${j}>
+        <div class="loader-container">
+          <img src="/icons/loader.svg" alt="loader" />
+        </div>
+        ${categories.map(getGauge).join("")}
+      </div>
+      <div>Open ${device} report</div>
+    </a>`;
+}
+
+function getGauge(category) {
+  return /* html */ `
+    <div class="gauge-wrapper">
+      <div class="gauge-svg-wrapper">
+        <svg class="gauge" viewBox="0 0 120 120">
+          <circle class="gauge-base" r="56" cx="60" cy="60" stroke-width="8"></circle>
+          <circle class="gauge-arc" r="56" cx="60" cy="60" stroke-width="8"></circle>
+        </svg>
+        <div class="gauge-percentage"></div>
+      </div>
+      <div class="gauge-label">${category}</div>
+    </div>`;
+}
+
+function updateGauge(gaugeWrapper, percentage) {
+  const c = percentage >= 90 ? ["#0c6", "#0E8F0F"] : ["#fa3", "#CC5023"];
+  const dash = (percentage / 100) * (percentage == 100 ? 350 : 346);
+  const style = `--gauge-color-1: ${c[0]}; --gauge-color-2: ${c[1]}; transform: none;`;
+  gaugeWrapper.querySelector(".gauge-svg-wrapper").setAttribute("style", style);
+  gaugeWrapper.querySelector(".gauge-arc").style.strokeDasharray = dash;
+  gaugeWrapper.querySelector(".gauge-percentage").textContent = percentage;
+  gaugeWrapper.style.opacity = 1;
+}
+
+async function runPagespeedAPI(name, device) {
+  const api = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed";
+  const params = {
+    url: `https://${name}.supit.k1k1.dev/`,
+    strategy: device.toUpperCase(),
+    key: APIkey,
+  };
+  const wtfGoogle = (c) => "category=" + c.replaceAll(" ", "_").toUpperCase();
+  const url = new URL(api);
+  url.search = new URLSearchParams(params).toString();
+  url.search += "&" + categories.map(wtfGoogle).join("&");
+  const json = await fetch(url).then((response) => response.json());
+  console.log(`FULL REPORT FOR ${name}, ${device}:`, json);
+  const _categories = json.lighthouseResult.categories;
+  return categories.map(
+    (c) => _categories[c.replaceAll(" ", "-").toLowerCase()].score * 100
+  );
+}

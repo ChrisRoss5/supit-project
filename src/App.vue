08c123ad667e35444cb4ecf545ffe19a10c0e78a
Chris <kristijan.ros@gmail.com>
2023-01-25 00:16:41 +0100

    fixed contact to stay alive

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
+<template>
+  <Header />
+  <RouterView />
+  <Footer />
+  <Contact v-if="isContactLoaded" />
+</template>
+
 <script setup lang="ts">
-import { defineAsyncComponent } from "vue";
+import { defineAsyncComponent, ref, watch } from "vue";
 import { useStore } from "@/store";
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 
 const Contact = defineAsyncComponent(() => import("./components/Contact.vue"));
 const store = useStore();
-</script>
+const isContactLoaded = ref(false); // keeps the component alive to allow transitions
 
-<template>
-  <Header />
-  <RouterView />
-  <Footer />
-  <Contact v-if="store.isContactOpen" />
-</template>
+watch(
+  () => store.isContactOpen,
+  () => (isContactLoaded.value = true)
+);
+</script>
